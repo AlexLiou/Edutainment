db6eb2e66504731e54a59a13857a487cd0795b9d
alexliou <alexliou0@gmail.com>
2022-04-15 12:07:25 -0400

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Alex Liou on 4/15/22.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
